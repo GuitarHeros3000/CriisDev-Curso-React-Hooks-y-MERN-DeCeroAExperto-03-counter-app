a780ecf669f4094fa5c480b5e81173d8a8d1a841
Cristian David Ramírez de la Rosa <crisronald3000@outlook.com>
2024-09-12 18:11:57 -0600

    Colocar estilos de CSS.

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,13 +1,9 @@
 
-const getResult = (a, b) => {
-    return a + b;
-};
-
 export const FirstApp = () => {
 
     return (
         <>
-            <h1>{getResult(1, 2)}</h1>
+            <h1>Cristian</h1>
             {/* <code>{JSON.stringify(newMessage)}</code> */}
             <p>Soy un subtítulo</p>
         </>
