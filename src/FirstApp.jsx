889d175a0f2b758265a27e5a5a6379e62ee04182
Cristian David Ramírez de la Rosa <crisronald3000@outlook.com>
2024-09-18 17:01:51 -0600

    PropTypes.

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,12 +1,21 @@
+import PropTypes from 'prop-types';
 
 export const FirstApp = ({ title, subtitle }) => {
+
+    // console.log(props);
 
     return (
         <>
             <h1>{title}</h1>
             {/* <code>{JSON.stringify(newMessage)}</code> */}
-            <p>{subtitle + 1}</p>
+            <p>{subtitle}</p>
         </>
     )
 
+}
+
+
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.number.isRequired,
 }
