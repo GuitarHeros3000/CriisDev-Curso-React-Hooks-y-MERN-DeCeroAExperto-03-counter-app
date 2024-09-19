8bc4e3355ff35512162c64285a1748c1d7b50806
Cristian David Ramírez de la Rosa <crisronald3000@outlook.com>
2024-09-19 16:51:07 -0600

    Jest - Expect - toBe.

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,6 +1,17 @@
 
-test('Esta prueba no debe de fallar', () => {
-    if (0 === 1) {
-        throw new Error('No se puede dividir entre cero.');
-    }
-})
+describe('Pruebas en <DemoComponent />', () => {
+
+    test('Esta prueba no debe de fallar', () => {
+
+        // 1. Inicialización
+        const message1 = 'Hola Mundo.';
+
+        // 2. Estímulo
+        const message2 = message1.trim();
+
+        // 3. Observar el comportamiento... esperado
+        expect(message1).toBe(message2);
+
+    });
+
+});
